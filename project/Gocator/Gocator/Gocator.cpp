2ecb4d9a548ff5b36741a6244c4776c915704f5e
Gabriele Tornatore <61290799+it9tst@users.noreply.github.com>
2021-12-08 17:54:28 +0100

    Update gocator

--- project/Gocator/Gocator/Gocator.cpp
+++ project/Gocator/Gocator/Gocator.cpp
@@ -1,7 +1,7 @@
 // Gocator.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
 
 //std c/c++
+#include <windows.h>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,54 +25,47 @@ int main(int argc, char** argv) {
 	std::cout << "Gocator example_2 running" << std::endl;
 
 	// construct Gocator API Library
-	if ((status = GoSdk_Construct(&api)) != kOK)
-	{
+	if ((status = GoSdk_Construct(&api)) != kOK) {
 		std::cout << "Error: GoSdk_Construct: " << status << std::endl;
 		return -1;
 	}
 
 	// construct GoSystem object
-	if ((status = GoSystem_Construct(&system, kNULL)) != kOK)
-	{
+	if ((status = GoSystem_Construct(&system, kNULL)) != kOK) {
 		std::cout << "Error: GoSystem_Construct: " << status << std::endl;
 		return -1;
 	}
 
 	// obtain GoSensor object by sensor IP address
 	kIpAddress_Parse(&ipAddress, SENSOR_IP);
-	if ((status = GoSystem_FindSensorByIpAddress(system, &ipAddress, &sensor)) != kOK)
-	{
+	if ((status = GoSystem_FindSensorByIpAddress(system, &ipAddress, &sensor)) != kOK) {
 		std::cout << "Error: GoSystem_FindSensorByIpAddress: " << status << std::endl;
 		return -1;
 	}
 
 	// create connection to GoSensor object
-	if ((status = GoSensor_Connect(sensor)) != kOK)
-	{
+	if ((status = GoSensor_Connect(sensor)) != kOK) {
 		std::cout << "Error: GoSensor_Connect: " << status << std::endl;
 		return -1;
 	}
 
 	// enable sensor data channel
-	if ((status = GoSystem_EnableData(system, kTRUE)) != kOK)
-	{
+	if ((status = GoSystem_EnableData(system, kTRUE)) != kOK) {
 		std::cout << "Error: GoSensor_EnableData: " << status << std::endl;
 		return -1;
 	}
 
 	// start Gocator sensor
-	if ((status = GoSystem_Start(system)) != kOK)
-	{
+	if ((status = GoSystem_Start(system)) != kOK) {
 		std::cout << "Error: GoSystem_Start: " << status << std::endl;
 		return -1;
 	}
 
 	//sleep for a while
-	sleep(5);
+	Sleep(5);
 
 	// stop Gocator sensor
-	if ((status = GoSystem_Stop(system)) != kOK)
-	{
+	if ((status = GoSystem_Stop(system)) != kOK) {
 		std::cout << "Error: GoSystem_Stop: " << status << std::endl;
 		return -1;
 	}
